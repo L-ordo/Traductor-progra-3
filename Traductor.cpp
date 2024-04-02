56de11448e8521966ccf09a38bad26d64750beee
L-ordonez10 <luis.pedro@bold.gt>
2024-04-01 21:32:58 -0600

    first commit

---
+++ Traductor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+using namespace std;
+
+void traductor();
+void cargarArchivo();
+void encriptarArchivo();
+void desencriptarArchivo();
+
+int op, optra;
+int main(){
+	
+		cout<<"==========================================";
+		cout<<"\n\t 1. Traductor";
+		cout<<"\n\t 2. Cargar un archivo";
+		cout<<"\n\t 3. Encriptar archivo";
+		cout<<"\n\t 4. Desencriptar archivo";
+		cout<<"\n\t 5. Salir\n";
+		cout<<"==========================================";
+		cin>>op;
+	
+}
+
+void traductor(){
+		cout<<"==========================================";
+		cout<<"\n\t Elija el lenguaje que desea traducir \n";
+		cout<<"==========================================";
+	
+}
